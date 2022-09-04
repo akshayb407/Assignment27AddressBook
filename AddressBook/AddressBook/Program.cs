0cc2b8c3863d6bef681d2adcc8e706f0a725fd3e
akshayb407 <akshayb407@gmail.com>
2022-09-04 18:27:38 +0530

    UC4

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,17 +6,22 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Address Book Program!");
-            ContactDetails createContacts = new ContactDetails();
-            createContacts.Contacts();
+          //  Console.WriteLine("Welcome To Address Book Program!");
+          //  ContactDetails createContacts = new ContactDetails();
+          //  createContacts.Contacts();
 
            // Console.WriteLine("Add New Contacts Program");
            // AddNewContacts addNewCon = new AddNewContacts();
            // addNewCon.addNewContacts();
 
-            Console.WriteLine("Update Existing Contact Program");
-            UpdateExistingContact update = new UpdateExistingContact();
-            update.UpdateContact();
+           // Console.WriteLine("Update Existing Contact Program");
+           // UpdateExistingContact update = new UpdateExistingContact();
+          //  update.UpdateContact();
+
+            Console.WriteLine("Delete a Contact Program");
+            DeleteContact deleteContact = new DeleteContact();
+            deleteContact.ContactDelete();
+            Console.ReadKey();
         }
     }
 }

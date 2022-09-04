b234310f20bcabd8b2b3bc76dcbabc27e3009065
akshayb407 <akshayb407@gmail.com>
2022-09-04 18:12:20 +0530

    UC2

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -9,6 +9,10 @@ namespace AddressBook
             Console.WriteLine("Welcome To Address Book Program!");
             ContactDetails createContacts = new ContactDetails();
             createContacts.Contacts();
+
+            Console.WriteLine("Add New Contacts Program");
+            AddNewContacts addNewCon = new AddNewContacts();
+            addNewCon.addNewContacts();
         }
     }
 }

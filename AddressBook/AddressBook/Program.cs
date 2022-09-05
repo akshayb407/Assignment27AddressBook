7dc66796c86a8bec696e5710e8916ceb7fdf628c
akshayb407 <akshayb407@gmail.com>
2022-09-05 19:13:08 +0530

    UC14 ReadWrite CSVFiles added

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -1,70 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-
 namespace AddressBook
 {
     class Program
     {
-        Address address1 = new Address("Akshay", "Bhagwat", "Katraj", "Pune",
-            "MH", "413709", 8087148746, "akshayb407@gmail.com");
-
         static void Main(string[] args)
         {
-            Program.FileExists();
-            Program.ReadAllLines();
-            Program.ReadAllText();
-           // Program.DeleteFile();
-            Program.CopyFile();
-        }
-        public static void FileExists()
-        {
-            String path = @"H:\All Asignment\Assignment27AddressBook\AddressBook\AddressBook\Contact.txt";
-            if (File.Exists(path))
-            {
-                Console.WriteLine("File Exists");
-            }
-        }
-        public static void ReadAllLines()
-        {
-            String path = @"H:\All Asignment\Assignment27AddressBook\AddressBook\AddressBook\Contact.txt";
-            String[] lines;
-            lines = File.ReadAllLines(path);
-            Console.WriteLine(lines[0]);
-            Console.WriteLine(lines[1]);
-        }
-        public static void ReadAllText()
-        {
-            String path = @"H:\All Asignment\Assignment27AddressBook\AddressBook\AddressBook\Contact.txt";
-            String lines;
-            lines = File.ReadAllText(path);
-            Console.WriteLine(lines);
-        }
-        public static void DeleteFile()
-        {
-            String path = @"H:\All Asignment\Assignment27AddressBook\AddressBook\AddressBook\Contacts.txt";
-            File.Delete(path);
-            if (File.Exists(path))
-            {
-                Console.WriteLine("File not deleted");
-            }
-            else
-            {
-                Console.WriteLine("File deleted");
-            }
-        }
-        public static void CopyFile()
-        {
-            String path = @"H:\All Asignment\Assignment27AddressBook\AddressBook\AddressBook\TextFile1.txt";
-            String copyPath = @"H:\All Asignment\Assignment27AddressBook\AddressBook\AddressBook\Contact.txt";
+            Console.WriteLine("Welcome to Address Book");
 
-            File.Copy(path, copyPath);
-            Console.ReadKey();
-        }
+           // #region UC14
+            ReadWriteCSV.CsvReadWrite();
+            //#endregion
 
+            
+        }
     }
-} 
-
-       
-    
-
+}
---
+++ AddressBook/AddressBook/ReadWriteCSV.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using System.Globalization;
+using System.IO;
+
+namespace AddressBook
+{
+    class ReadWriteCSV
+    {
+        public class AddressData
+        {
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public string Email { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Zip { get; set; }
+        }
+
+        public static void CsvReadWrite()
+        {
+            string importPath = @"H:\All Asignment\Assignment27AddressBook\AddressBook\AddressBook\addressBook.csv";
+            string exportPath = @"H:\All Asignment\Assignment27AddressBook\AddressBook\AddressBook\contactList.csv";
+            using (var reader = new StreamReader(importPath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Contacts: ");
+
+                int count = 1;
+                foreach (var contact in records)
+                {
+
+                    Console.WriteLine(count + ".  " + contact.Name + "\t" + contact.Phone + "\t" + contact.Email + "\t" + contact.Address
+                                  + "\t" + contact.City + "\t" + contact.State + "\t" + contact.Zip);
+                    count++;
+                }
+
+                using (var writer = new StreamWriter(exportPath))
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(records);
+                }
+            }
+        }
+
+    }
+}
